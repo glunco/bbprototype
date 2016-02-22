f3adb695a9cab112201605e7af728fa82322a8cd
Sergio Díaz <seich@martianwabbit.com>
2016-02-22 16:44:44 -0600

    Angularific.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,38 @@
-angular
-.module('course', ['ui.router'])
-	.factory('$announcements', function($http) {return {
-			get: function(id) {
-				return $http.get('/fixtures/' + id + '.json')
-			},
+angular.module('course', ['ui.router'])
 
-			getAll: function() {
-				return $http.get('/fixtures/all.json')
-			}
-		}
-	});
+.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/overview');
+
+	$stateProvider
+		.state('overview', {
+			url: "/overview",
+			templateUrl: 'include/overview.html'
+		})
+		
+		.state('announcements', {
+			url: "/announcements",
+			templateUrl: 'include/announcements.html'
+		})
+		
+		.state('discussion', {
+			url: "/discussion",
+			templateUrl: 'include/discussion.html'
+		})
+		
+		.state('syllabus', {
+			url: "/syllabus",
+			templateUrl: 'include/syllabus.html'
+		})
+		
+		.state('resources', {
+			url: "/resources",
+			templateUrl: 'include/resources.html'
+		})
+
+		.state('assignments', {
+			url: "/assignments",
+			templateUrl: 'include/assignments.html'
+		})
+});
+
+$(document).foundation();
